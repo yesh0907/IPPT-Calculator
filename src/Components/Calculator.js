714508becc6fe46fa64213514821d449751dabae
Yesh Chandiramani <yesh0907@hotmail.com>
2017-11-25 20:14:16 +0800

    implement all logic

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -9,25 +9,36 @@ import PushUps from './PushUps';
 import SitUps from './SitUps';
 import Run from './Run';
 
-const PUSHUPS_SCORING = {
-  '30': 16, '37': 18, '22': 11, '36': 18, '60': 25, '19': 8, '35': 18, '34': 17, '23': 12, '24': 13, '25': 14, '26': 15,
-  '27': 15, '20': 9, '21': 10, '48': 21, '49': 22, '46': 21, '47': 21, '44': 20, '45': 21, '42': 20, '43': 20, '40': 20,
-  '41': 20, '1': 0, '3': 0, '2': 0, '5': 0, '4': 0, '7': 0, '6': 0, '9': 0, '8': 0, '18': 6, '28': 15,
-  '13': 0, '12': 0, '15': 1, '29': 16, '14': 0, '11': 0, '10': 0, '39': 19, '38': 19, '59': 24, '58': 24, '17': 4,
-  '16': 2, '55': 23, '54': 23, '57': 24, '56': 23, '51': 22, '50': 22, '53': 23, '52': 22, '33': 17, '32': 17, '31': 16
-}
 
 class Calculator extends Component {
   render() {
-    const { actions } = this.props;
+    const { actions, state } = this.props;
+    const { score } = state;
+
+    let result = null;
+    if (score !== -1) {
+      if (score >= 90) result = (<h1>Commando/Guards Gold!<br />Score: <b>{score}</b></h1>);
+      else if (score >= 85) result = (<h1>You got Gold!<br />Score: <b>{score}</b></h1>);
+      else if (score >= 75) result = (<h1>You got Silver!<br />Score: <b>{score}</b></h1>);
+      else if (score >= 61) result = (<h1>You Passed (skip IPPT lah)!<br />Score: <b>{score}</b></h1>);
+      else result = (<h1>You Failed :(<br />Score: <b>{score}</b></h1>)
+    }
+
     return (
-      <div>
-        <Form layout="inline" id="calculator">
+      <div id="calculator">
+        <Form layout="inline">
           <PushUps updatePushUpsReps={actions.updatePushUpsReps} />
           <SitUps updateSitUpsReps={actions.updateSitUpsReps} />
           <Run updateRunTime={actions.updateRunTime} />
-          <Button type="primary" shape="circle" icon="calculator" id="calculate"></Button>
+          <Button
+            type="primary"
+            shape="circle"
+            icon="calculator"
+            id="calculate"
+            onClick={actions.calculateScore}
+          />
         </Form>
+        {result}
       </div>
     );
   }
@@ -35,7 +46,7 @@ class Calculator extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    calculator: state.calculator
+    state
   }
 }
 
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 export const UPDATE_PUSHUPS_REPS = 'UPDATE_PUSHUPS_REPS';
 export const UPDATE_SITUPS_REPS = 'UPDATE_SITUPS_REPS';
 export const UPDATE_RUN_TIME = 'UPDATE_RUN_TIME';
+export const CALCULATE_SCORE = 'CALCULATE_SCORE';
 
 export function updatePushUpsReps(reps) {
   return {
@@ -20,5 +21,11 @@ export function updateRunTime(time) {
   return {
     type: UPDATE_RUN_TIME,
     payload: time
+  }
+}
+
+export function calculateScore() {
+  return {
+    type: CALCULATE_SCORE
   }
 }
